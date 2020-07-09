a013b2886d5767a5af2a64ef57c46e6fb8dbda3b
Aaron Sawyer <10297223+aarondevon@users.noreply.github.com>
2020-07-09 17:40:59 -0500

    counter app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,27 +4,110 @@ console.log('app.js is running.');
 
 // JSX JavaScript XML
 // let template = <p>This is JSX from app.js</p>;
-var template = React.createElement('h1', null);
+var app = {
+  title: 'Indecision App',
+  subtitile: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
+};
 
-var templateTwo = React.createElement(
+var template = React.createElement(
   'div',
   null,
   React.createElement(
     'h1',
     null,
-    'Aaron Sawyer'
+    app.title
+  ),
+  app.subtitile && React.createElement(
+    'p',
+    null,
+    app.subtitile
   ),
   React.createElement(
     'p',
     null,
-    'Age: 35'
+    app.options.length > 0 ? 'Here are you options' : 'No options'
   ),
   React.createElement(
-    'p',
+    'ol',
     null,
-    'Location: Minnetonka'
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
   )
 );
+
+var user = {
+  name: 'Aaron',
+  age: 35,
+  location: 'Minnetonka'
+};
+
+function geLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+var count = 0;
+
+var addOne = function addOne() {
+  count++;
+  renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+  count--;
+  renderCounterApp();
+};
+
+var reset = function reset() {
+  count = 0;
+  renderCounterApp();
+};
+
+var renderCounterApp = function renderCounterApp() {
+  var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      'Count: ',
+      count
+    ),
+    React.createElement(
+      'button',
+      { onClick: addOne },
+      '+1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: minusOne },
+      '-1'
+    ),
+    React.createElement(
+      'button',
+      { onClick: reset },
+      'Reset'
+    )
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -2,15 +2,66 @@ console.log('app.js is running.');
 
 // JSX JavaScript XML
 // let template = <p>This is JSX from app.js</p>;
-let template = <h1></h1>;
+const app = {
+  title: 'Indecision App',
+  subtitile: 'Put your life in the hands of a computer',
+  options: ['One', 'Two'],
+};
 
-let templateTwo = (
+const template = (
   <div>
-    <h1>Aaron Sawyer</h1>
-    <p>Age: 35</p>
-    <p>Location: Minnetonka</p>
+    <h1>{app.title}</h1>
+    {app.subtitile && <p>{app.subtitile}</p>}
+    <p>{app.options.length > 0 ? 'Here are you options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
   </div>
 );
+
+const user = {
+  name: 'Aaron',
+  age: 35,
+  location: 'Minnetonka',
+};
+
+function geLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+let count = 0;
+
+const addOne = () => {
+  count++;
+  renderCounterApp();
+};
+
+const minusOne = () => {
+  count--;
+  renderCounterApp();
+};
+
+const reset = () => {
+  count = 0;
+  renderCounterApp();
+};
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+renderCounterApp();
