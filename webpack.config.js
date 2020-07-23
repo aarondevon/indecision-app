4d1f44b77bed3c094cda00a616149d85998b36b5
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-07-23 16:31:10 -0500

    install loaders for scss

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
     ],
   },
   devtool: 'cheap-module-source-map',
