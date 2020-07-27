4104e244767ae9c7fdd255df69bf8340aab48b4f
Aaron <10297223+aarondevon@users.noreply.github.com>
2020-07-27 10:37:52 -0500

    add class names

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,10 +7,16 @@ const OptionModal = (props) => {
       isOpen={!!props.selectedOption}
       onRequestClose={props.onCloseModal}
       contentLabel="Selected Option"
+      closeTimeoutMS={200}
+      className="modal"
     >
-      <h3>Selected Option</h3>
-      {props.selectedOption && <p>{props.selectedOption}</p>}
-      <button onClick={props.onCloseModal}>Close Modal</button>
+      <h3 className="modal__title">Selected Option</h3>
+      {props.selectedOption && (
+        <p className="modal__body">{props.selectedOption}</p>
+      )}
+      <button className="button" onClick={props.onCloseModal}>
+        Close Modal
+      </button>
     </Modal>
   );
 };
